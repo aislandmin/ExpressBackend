a12745469856cfa03dfd8c982f27d8fd237d8e23
jianglai <jianglai3303@gmail.com>
2021-06-29 20:28:57 -0400

    create login api

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser')
 const app = express();
+const jsonParser = bodyParser.json()
 
-app.get('/a', (req, res) => {
-  res.send('Hello World!')
-})
-app.post('/a', (req, res) => {
-  res.json({ foo: 'bar' })
-})
 const getFile = (url) => {
   return new Promise((resolve, reject) => {
     fs.readFile(url, 'utf-8', (err, data) => {
@@ -19,31 +15,18 @@ const getFile = (url) => {
     });
   });
 }
-app.get('/api1', async (req, res) => {
-  // #1
-  // fs.readFile('./1.json', 'utf-8', (err, data) => {
-  //   if (err) {
-  //     console.error(err)
-  //     return
-  //   }
-  //   fs.readFile('./2.json', 'utf-8', (err2, data2) => {
-  //     const result = JSON.parse(data).concat(JSON.parse(data2));
-  //     res.json(result);
-  //   });
-  // });
-
-  //#2
-  // getFile('./1.json').then(data1=>{
-  //   console.log(data1);
-  //   return getFile('./2.json');
-  // }).then(data2=>{
-  //   console.log(data2);
-  // })
-
+app.post('/login', jsonParser, async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(req.body)
   //#3
-  const data1 = await getFile('./1.json');
-  const data2 = await getFile('./2.json');
-  res.json(JSON.parse(data1).concat(JSON.parse(data2)))
+  let data;
+  if(username==='wjiang' && password ==='123456'){
+    data = await getFile('./mock/login.json');
+  }else{
+    data = await getFile('./mock/login_fail.json');
+  }
+  res.json(JSON.parse(data))
 })
 app.listen(3333, () => {
   console.log(`Example app listening at http://localhost:3333`)
