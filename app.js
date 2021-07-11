86dce968190d61c7b4864216865aa9b8f3d7c0f2
jianglai <jianglai3303@gmail.com>
2021-07-11 14:37:31 -0400

    add order apis

--- app.js
+++ app.js
@@ -23,6 +23,131 @@ app.post('/api/order/detail', jsonParser, async(req, res)=>{
   data = await getFile('./mock/order_detail.json');
   res.json(JSON.parse(data))
 })
+app.post('/api/calculate',jsonParser, async(req, res)=>{
+  const {pLat, pLng} = req.body.pickup;
+  const {dLat, dLng} = req.body.delivery;
+  const type = req.body.type;
+  if(!pLat || !pLng){
+    res.json({
+      code: 500,
+      msg: 'parameter pickup is missing'
+    })
+  }
+  if(!dLat || !dLng){
+    res.json({
+      code: 500,
+      msg: 'parameter delivery is missing'
+    })
+  }
+  if(!type){
+    res.json({
+      code: 500,
+      msg: 'parameter type is missing'
+    })
+  }
+  res.json({
+    code: 200,
+    data: {
+      book_fee: 2,
+      base_fee: 11.2,
+    }
+  })
+})
+app.post('/api/order/create',jsonParser, async(req, res)=>{
+  const money = req.body.money;
+  const tips = req.body.tips;
+  const username= req.body.username;
+  const {pLat, pLng, pAddress} = req.body.pickup;
+  const {dLat, dLng, dAddress} = req.body.pickup;
+  const remark = req.body.remark;
+  const pickup_customer = req.body.pickup_customer;
+  const pickup_phone = req.body.pickup_phone;
+  const delivery_customer = req.body.delivery_customer;
+  const delivery_phone = req.body.delivery_phone;
+  if(!money){
+    res.json({
+      code: 500,
+      msg: 'parameter type is missing'
+    })
+  }
+  if(isNaN(money)){
+    res.json({
+      code: 500,
+      msg: 'money format is incorrect'
+    })
+  }
+  if(!tips){
+    res.json({
+      code: 500,
+      msg: 'parameter type is missing'
+    })
+  }
+  if(isNaN(tips)){
+    res.json({
+      code: 500,
+      msg: 'tips format is incorrect'
+    })
+  }
+  if(!username){
+    res.json({
+      code: 500,
+      msg: 'parameter type is missing'
+    })
+  }
+  if(!pLat || !pLng || !pAddress){
+    res.json({
+      code: 500,
+      msg: 'parameter pickup is missing'
+    })
+  }
+  if(!dLat || !dLng || !dAddress){
+    res.json({
+      code: 500,
+      msg: 'parameter delivery is missing'
+    })
+  }
+  if(!type){
+    res.json({
+      code: 500,
+      msg: 'parameter type is missing'
+    })
+  }
+  if(remark && remark.length > 100){
+    res.json({
+      code: 500,
+      msg: 'maximum length of remark is 100'
+    })
+  }
+  if(!pickup_customer){
+    res.json({
+      code: 500,
+      msg: 'parameter pickup_customer is missing'
+    })
+  }
+  if(!pickup_phone){
+    res.json({
+      code: 500,
+      msg: 'parameter pickup_phone is missing'
+    })
+  }
+  if(!delivery_customer){
+    res.json({
+      code: 500,
+      msg: 'parameter delivery_customer is missing'
+    })
+  }
+  if(!delivery_phone){
+    res.json({
+      code: 500,
+      msg: 'parameter delivery_phone is missing'
+    })
+  }
+  res.json({
+    code: 200,
+    msg: 'order created successfully',
+    order_id: 123
+  })
+})
 app.post('/login', jsonParser, async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
